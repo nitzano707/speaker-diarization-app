783d5eb4c034cc3af908cae5ed1d0fad52e3c77c
nitzano707 <nitzan.orot@gmail.com>
2024-11-23 19:21:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ client = Client(SPACE_NAME)
 
 @app.route('/')
 def index():
-    return render_template('index.html')  # ממשק ה-Frontend
+    return render_template('index.html')  # קובץ HTML לממשק משתמש
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -35,4 +35,4 @@ def upload():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
